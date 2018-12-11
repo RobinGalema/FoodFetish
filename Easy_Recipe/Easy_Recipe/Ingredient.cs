a8f03d1381ee4bcfeac6e69e37ab43c4ab893c2c
Jesper <bawhaa@gmail.com>
2018-12-11 11:34:11 +0100

    The basics of Class ingredient made

---
+++ Easy_Recipe/Easy_Recipe/Ingredient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy_Recipe
+{
+    class Ingredient
+    {
+        // The name of the ingredient 
+        private string name;
+        // The amount that is needed of the ingredient
+        private int amount;
+
+        public Ingredient(string name, int amount)
+        {
+            // giving the name of ingredient
+            this.name = name;
+            // getting the amount that is required
+            this.amount = amount;
+        }
+    }
+}
