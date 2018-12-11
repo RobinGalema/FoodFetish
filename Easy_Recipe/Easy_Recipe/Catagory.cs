e14ae4a68fe43d02bacaf8342414a0949e20affb
Jesper <bawhaa@gmail.com>
2018-12-11 11:36:54 +0100

    the basics of class ingredient made

---
+++ Easy_Recipe/Easy_Recipe/Catagory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy_Recipe
+{
+    class Catagory
+    {
+        private string name;
+
+        public Catagory(string name)
+        {
+            this.name = name;
+        }
+    }
+}
